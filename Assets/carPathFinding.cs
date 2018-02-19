29de1a90d6fba71eaba62488bb38639a017ea06a
pankaj <pnegi8958@gmail.com>
2018-02-20 01:43:33 +0530

    Intial Commit with sorce code
    Contain the assets and script for car motion and traffic signal with pedestrial Click the Button to enable the Red light

---
+++ Assets/carPathFinding.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class carPathFinding : MonoBehaviour {
+    NavMeshAgent agent;
+    public Transform[] cornerTransPos;
+    Vector3[] cornerPos;
+    Vector3 targetDis;
+    public float rotationDir;
+	// Use this for initialization
+	void Start () {
+        agent = this.GetComponent<NavMeshAgent>();
+        if(cornerTransPos != null && cornerTransPos.Length > 0)
+        {
+            cornerPos = new Vector3[cornerTransPos.Length];
+            for(int i = 0; i < cornerTransPos.Length; i++)
+            {
+                cornerPos[i] = cornerTransPos[i].position;
+            }
+        }
+        motionorDestinationSetforCar(false);
+    }
+
+    float distance = 0;
+    int corner = 0;
+	// Update is called once per frame
+	void Update () {
+		if(agent)
+        {
+            distance = Vector3.Distance(transform.position, targetDis);
+          //  Debug.Log("distance----"+distance);
+            if(distance < 0.12f)
+                motionorDestinationSetforCar ();
+        }
+	}
+
+    void motionorDestinationSetforCar (bool rotate = true)
+    {
+        if (corner > 3)
+            corner = 0;
+        if(rotate)
+            transform.Rotate(new Vector3(0, rotationDir , 0), Space.World);
+        targetDis = cornerPos[corner];
+        agent.SetDestination(targetDis);
+        corner++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      //  Debug.Log("on trigger... car"+ other.tag);
+       if(other.tag.Equals ("trafficLight"))
+        {
+            if(other.GetComponent<trafficLightSystem>().redLightEnabled)
+            {
+                agent.enabled = false;
+            }
+        } 
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+      //  Debug.Log("on trigger stay... car" + other.tag);
+        if (other.tag.Equals("trafficLight"))
+        {
+            if (other.GetComponent<trafficLightSystem>().redLightEnabled)
+            {
+                agent.enabled = false;
+            }
+            else
+            {
+                agent.enabled = true;
+                corner--;
+                motionorDestinationSetforCar(false);
+            }
+        }
+
+    }
+}
---
+++ Assets/trafficLightSystem.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trafficLightSystem : MonoBehaviour {
+
+    public MeshRenderer redLight;
+    public MeshRenderer orangeLight;
+    public MeshRenderer greenLight;
+    public GameObject pedestrianTowalk;
+    public bool redLightEnabled = false;
+
+    void Start ()
+    {
+        ChangesInTrafficsignal("GREEN");
+    }
+
+    void Update ()
+    {
+        if (redLightEnabled)
+        {
+            movePedestrianForwardOrReset();
+        }
+        else if (redLight.enabled)
+        {
+            movePedestrianForwardOrReset(true);
+            ChangesInTrafficsignal("GREEN");
+        }
+    }
+
+    void ChangesInTrafficsignal (string signal)
+    {
+        switch (signal)
+        {
+            case "GREEN":
+                greenLight.enabled = true;
+                redLight.enabled = false;
+                orangeLight.enabled = false;
+                break;
+            case "RED":
+                greenLight.enabled = false;
+                redLight.enabled = true;
+                orangeLight.enabled = false;
+                break;
+            case "ORANGE":
+                greenLight.enabled = false;
+                redLight.enabled = false;
+                orangeLight.enabled = true;
+                break;
+        }
+    }
+
+    bool isTouchBlocked = false;
+    public void enableAndDisableRedSignal ()
+    {
+        if (isTouchBlocked)
+            return;
+
+        isTouchBlocked = true;
+        Invoke("resetTouch",0.5f);
+        if(redLightEnabled)
+        {
+            redLightEnabled = false;
+        } else
+        {
+            ChangesInTrafficsignal("RED");
+            redLightEnabled = true;
+        }
+    }
+
+    void resetTouch ()
+    {
+        isTouchBlocked = false;
+    }
+
+    void movePedestrianForwardOrReset (bool placeToOriginalPos = false)
+    {
+        Vector3 currentLoc = pedestrianTowalk.transform.position;
+        if (currentLoc.z < 0)
+            placeToOriginalPos = true;
+        if (placeToOriginalPos)
+        {
+            currentLoc.z = 4.8f;
+            pedestrianTowalk.transform.localPosition = currentLoc;
+            return;
+        }
+        Vector3 nextPos = currentLoc;
+        pedestrianTowalk.transform.Translate(Vector3.forward * Time.deltaTime);
+        //pedestrianTowalk.transform.localPosition = Vector3.Lerp(transform.localPosition, nextPos, Time.deltaTime * 0.5f);
+    }
+
+}
